26108d1f73d57763fc5a3e9b440bda1e33a3fcb8
arjunsuresh <arjunnsuresh@gmail.com>
2023-02-09 02:16:05 -0500

    my first commit

---
+++ src/main/python/01print.py
@@ -0,0 +1,16 @@
+print("hello world")
+print("hello world", "I am Arjun")
+print(1+4)
+print(1*5)
+greetings = "hello"
+name = "Arjun"
+print(greetings, name)
+print(greetings + name)
+print(greetings + ' ' + name)
+print(type(greetings))
+A = 24.05
+print(type(A))
+# data types in python are integer, strings, float, list, tuples, sets, dictionaries
+#A = abc
+A = 345
+print(A)
