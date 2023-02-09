f5d2116f18ab8fb7a414d4f5a61f6842805899a4
arjunsuresh <arjunnsuresh@gmail.com>
2023-02-09 02:18:17 -0500

    my first commit

---
+++ src/main/python/03operators.py
@@ -0,0 +1,19 @@
+a=4
+b=6
+print('sum is:', a+b)
+print('multiple is:', a*b)
+print('division is:', a/b)
+print('subtraction is:', a-b)
+print('modulus is', a%b)
+print('power is', a**b)
+print()
+print('integer division is:', a//b)
+#operator precedence
+#PEMDAS (Parenthisis, exponents , multiplication/division, addition/substraction
+#BEDMAS (brackets, exponents , division/mulitplication, addition/substraction
+#BODMAS (brackets, order , division/mulitplication, addition/substraction
+#BIDMAS (brackets, index , division/mulitplication, addition/substraction
+print(a+b/3-4*12)
+print(a+(b/3)-(4*12))
+print((((a+b)/3)-4)*12)
+print(((a+b)/3-4)*12)
