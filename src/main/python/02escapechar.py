c6cf8ba263d168f059f7c44c24a0ea73bcbad06d
arjunsuresh <arjunnsuresh@gmail.com>
2023-02-09 02:17:44 -0500

    my first commit

---
+++ src/main/python/02escapechar.py
@@ -0,0 +1,15 @@
+splitstring = "this string has been split \nover \nseparate lines"
+print(splitstring)
+splitstring = "this string has been split \\nover \\nseparate lines"
+print(splitstring)
+name = 'Neil O\'Mconnell'
+print(name)
+splitstring = "this string has been split " \
+              "over " \
+              "separate lines"
+print(splitstring)
+splitstring = """this string has been split
+over
+separate lines
+"""
+print (splitstring)
