54b4db849077d03933d8f40b8e32fe2c53e528bb
arjunsuresh <arjunnsuresh@gmail.com>
2023-02-17 12:53:52 -0500

    my first commit

---
+++ src/main/python/10continueBreak.py
@@ -0,0 +1,44 @@
+shoppingList = ["milk","fruits","vegetables","beans"]
+for i in shoppingList:
+    if i != "fruits":
+        print(i)
+    else:
+        continue
+        print("My name is Arjun")
+print("-"*80)
+
+shoppingList = ["milk","fruits","vegetables","beans"]
+for i in shoppingList:
+    if i != "fruits":
+        print(i)
+    else:
+        pass
+        print("My name is Arjun")
+print("-"*80)
+
+shoppingList = ["milk","fruits","vegetables","beans"]
+for i in shoppingList:
+    if i != "fruits":
+        print(i)
+    else:
+        break
+        print("My name is Arjun")
+
+for i in range(1,101):
+    if i%2 == 0:
+        print(i)
+    else:
+        continue
+
+for i in range(1, 101):
+    if i >50:
+        break
+    elif i % 2 == 0:
+        print(i)
+
+    else:
+        continue
+
+
+
+
